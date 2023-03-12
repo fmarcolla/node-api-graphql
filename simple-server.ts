b92425c427e3e4e89522c6cb194c2fee7c729097
Marcolla <felipem.marcolla@gmail.com>
2023-03-12 20:14:45 -0300

    Criado rota para cadastro e busca de usuarios

--- simple-server.ts
+++ simple-server.ts
@@ -1,19 +1,46 @@
 import { ApolloServer, gql } from 'apollo-server';
+import crypto from 'node:crypto';
 
 const typeDefs = gql`
-    type Query {
-        helloWorld: String!
+    type User {
+        id: String!
+        name: String!
     }
 
-    # type Mutation{}
+    type Query {
+        users: [User!]!
+    }
+
+    type Mutation {
+        createUser(name: String!): User!
+    }
 `
+
+interface User {
+    id: string
+    name: string
+}
+
+const users: User[] = [];
 
 const server = new ApolloServer({
     typeDefs,
     resolvers: {
         Query: {
-            helloWorld: () => {
-                return 'Hello World'
+            users: () => {
+                return users;
+            }
+        },
+        Mutation: {
+            createUser: (parent, args, ctx) => {
+                const user = {
+                    id: crypto.randomUUID(),
+                    name: args.name
+                };
+
+                users.push(user);
+
+                return user;
             }
         }
     }
